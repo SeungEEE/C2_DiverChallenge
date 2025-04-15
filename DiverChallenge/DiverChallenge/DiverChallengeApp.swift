1f05ccd517fb9abf7d91bfa8d877f2166ab99d87
Seungjin Lee <sjin3687@gmail.com>
2025-04-15 14:38:09 +0900

    🎉 프로젝트 생성

---
+++ DiverChallenge/DiverChallenge/DiverChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiverChallengeApp.swift
+//  DiverChallenge
+//
+//  Created by 이승진 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DiverChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
