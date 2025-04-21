63f3575de51fe31731a718bbbe22c5bf07ce87ec
Seungjin Lee <sjin3687@gmail.com>
2025-04-21 19:46:29 +0900

    [Refactor] DivierChallengeApp
    SwiftData

--- DiverChallenge/DiverChallenge/DiverChallengeApp.swift
+++ DiverChallenge/DiverChallenge/DiverChallengeApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct DiverChallengeApp: App {
@@ -13,5 +14,6 @@ struct DiverChallengeApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [DivingBook.self, DivingDailyLog.self])
     }
 }
