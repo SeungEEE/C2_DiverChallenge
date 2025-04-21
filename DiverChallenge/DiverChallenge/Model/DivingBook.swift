a7d6785e06023256f7e0498970e5b9b51559736c
Seungjin Lee <sjin3687@gmail.com>
2025-04-21 19:46:07 +0900

    [Feat] DivingBook 구현

---
+++ DiverChallenge/DiverChallenge/Model/DivingBook.swift
@@ -0,0 +1,28 @@
+//
+//  DivingBook.swift
+//  DiverChallenge
+//
+//  Created by 이승진 on 4/21/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@Model
+class DivingBook {
+    @Attribute(.unique) var id: UUID = UUID()
+    var title: String              // 도감 제목
+    var goal: String               // 도감 목표
+    var startDate: Date            // 시작 날짜
+    var endDate: Date              // 종료 날짜
+    var createdAt: Date = Date()   // 현재 날짜
+    
+    @Relationship(deleteRule: .cascade) var dailyLogs: [DivingDailyLog] = []
+
+    init(title: String, goal: String, startDate: Date, endDate: Date) {
+        self.title = title
+        self.goal = goal
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+}
