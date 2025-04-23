e63e742412ba033b981435a4fb42935163a2aa61
Seungjin Lee <sjin3687@gmail.com>
2025-04-23 21:04:08 +0900

    [Refactor] DivingLogView
    Navigation 수정, dayNumber 수정

--- DiverChallenge/DiverChallenge/Module/Log/DivingLogView.swift
+++ DiverChallenge/DiverChallenge/Module/Log/DivingLogView.swift
@@ -9,41 +9,70 @@ import SwiftUI
 import SwiftData
 
 struct DivingLogView: View {
+    /// SwiftData 바인딩 (양방향)
     @Bindable var log: DivingDailyLog
+    
+    /// SwiftData 컨텍스트
     @Environment(\.modelContext) private var modelContext
+    
+    /// 네비게이션 back 버튼 위한 환경 변수
+    @Environment(\.dismiss) private var dismiss
+    
+    /// 새로운 도감인지 판단
+    let isNew: Bool
     
     /// 텍스트필드 선택시 Focus해주기 위한 상태 변수
     @FocusState private var goalIsFocused: Bool
     @FocusState private var wordIsFocused: Bool
     
+    /// 오늘의 목표
     @State private var goal: String
+    
+    /// 오늘의 한마디
     @State private var word: String
     
     /// 선택된 아이템 종류를 저장하는 상태 변수
     @State private var newModeType: ModeType
     
+    /// 체크 버튼 클릭 상태 저장하는 변수
+    @State private var isChecked: Bool
+    
     /// 초기 상태로 세팅
-    init(log: DivingDailyLog) {
+    init(log: DivingDailyLog, isNew: Bool = false) {
         self._log = Bindable(wrappedValue: log)
         self._goal = State(initialValue: log.todayGoal)
         self._word = State(initialValue: log.todayNote ?? "")
         self._newModeType = State(initialValue: log.todayMood)
+        self.isNew = isNew
+        self._isChecked = State(initialValue: log.isDone)
     }
     
     var body: some View {
-        VStack(spacing: 16) {
-            TopView
+        VStack {
+            CustomNavigationBar(
+                title: "다이빙 로그",
+                showBackButton: true
+            )
             
-            goalView
-            
-            wordView
-            
-            moodView
-            
-            bottomView
+            ScrollView {
+                VStack(spacing: 24) {
+                    
+                    TopView
+                    
+                    goalView
+                    
+                    wordView
+                    
+                    moodView
+                    
+                    bottomView
+                }
+                .safeAreaPadding(EdgeInsets(top: 32, leading: 16, bottom: 0, trailing: 16))
+                
+            }
+            .hideKeyboardOnTap()
         }
-        .safeAreaPadding(EdgeInsets(top: 32, leading: 16, bottom: 0, trailing: 16))
-        .navigationTitle(log.book.title)
+        .navigationBarHidden(true)
     }
     
     /// 전체 목표 + 남은 날짜를 보여주는 뷰
@@ -52,28 +81,27 @@ struct DivingLogView: View {
             HStack {
                 Label {
                     Text(log.book.goal)
-                        .font(.pretendard(type: .semibold, size: 30))
+                        .font(.pretendard(type: .semibold, size: 28))
                     
                 } icon: {
-                    Image(systemName: "rosette")
-                        .resizable()
-                        .frame(width: 24, height: 30)
+                    Text("📌")
+                        .font(.system(size: 28))
                 }
-                .foregroundStyle(.main)
+                .foregroundStyle(.black)
                 
                 Spacer()
                 
                 Text("Day \(dayNumber)")
-                    .font(.pretendard(type: .semibold, size: 16))
+                    .font(.pretendard(type: .semibold, size: 18))
                     .foregroundStyle(.main)
                     .offset(y: 10)
+                
             }
             
             Rectangle()
                 .frame(maxWidth: .infinity)
-                .frame(height: 3)
-                .foregroundStyle(.main)
-            
+                .frame(height: 1)
+                .foregroundStyle(.navy01)
         }
     }
     
@@ -82,14 +110,14 @@ struct DivingLogView: View {
         VStack(alignment: .leading, spacing: 16) {
             Text("🏆 오늘의 목표")
                 .font(.pretendard(type: .bold, size: 20))
-                .foregroundStyle(.main)
+                .foregroundStyle(.black)
             
             HStack {
                 Button {
-                    
+                    isChecked.toggle()
                 } label: {
-                    Image(systemName: "square")
-                        .foregroundStyle(.gray01)
+                    Image(systemName: isChecked ? "checkmark.square.fill" : "square")
+                        .foregroundStyle(isChecked ? .main : .gray01)
                 }
                 
                 TextField("오늘의 목표는 무엇인가요?", text: $goal)
@@ -112,7 +140,7 @@ struct DivingLogView: View {
         VStack(alignment: .leading, spacing: 16) {
             Text("💬 오늘의 한마디")
                 .font(.pretendard(type: .bold, size: 20))
-                .foregroundStyle(.main)
+                .foregroundStyle(.black)
             
             ZStack(alignment: .topLeading) {
                 if word.isEmpty {
@@ -132,7 +160,7 @@ struct DivingLogView: View {
                     .cornerRadius(4)
                     .overlay(
                         RoundedRectangle(cornerRadius: 4)
-                            .stroke(wordIsFocused ? Color.main : Color.gray01, lineWidth: 1)
+                            .stroke(wordIsFocused ? .main : Color.gray01, lineWidth: 1)
                     )
             }
             
@@ -146,7 +174,7 @@ struct DivingLogView: View {
         VStack(alignment: .leading, spacing: 16) {
             Text("😀 오늘의 기분")
                 .font(.pretendard(type: .bold, size: 20))
-                .foregroundStyle(.main)
+                .foregroundStyle(.black)
             
             // 기분 선택 Picker
             Picker("무드 종류", selection: $newModeType) {
@@ -159,30 +187,36 @@ struct DivingLogView: View {
             
             Rectangle()
                 .frame(maxWidth: .infinity)
-                .frame(height: 3)
-                .foregroundStyle(.main)
+                .frame(height: 1)
+                .foregroundStyle(.navy01)
         }
     }
     
     /// 오늘 날짜 + 저장하기 버튼
     private var bottomView: some View {
-        VStack(alignment: .trailing) {
+        VStack(alignment: .trailing, spacing: 30) {
             Text(formattedDate)
                 .font(.pretendard(type: .semibold, size: 16))
-            
-            Spacer()
+                .foregroundStyle(.navy01)
             
             MainButton(buttonType: .save) {
                 log.todayGoal = goal
                 log.todayNote = word
                 log.todayMood = newModeType
+                log.isDone = isChecked
+                
+                if isNew {
+                    modelContext.insert(log)
+                    log.book.dailyLogs.append(log)
+                }
                 
                 try? modelContext.save()
+                dismiss()
             }
         }
     }
     
-    // MARK: - 날짜/Day 계산 함수
+    /// 날짜/Day 계산 함수
     private var formattedDate: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy.MM.dd(E)"
@@ -190,8 +224,13 @@ struct DivingLogView: View {
         return formatter.string(from: log.date)
     }
     
+    /// 디데이
     private var dayNumber: Int {
-        Calendar.current.dateComponents([.day], from: log.book.startDate, to: log.date).day.map { $0 + 1 } ?? 1
+        let start = Calendar.current.startOfDay(for: log.book.startDate)
+        let target = Calendar.current.startOfDay(for: log.date)
+        
+        let diff = Calendar.current.dateComponents([.day], from: start, to: target).day ?? 0
+        return diff + 1 // Day 1부터 시작
     }
 }
 
@@ -201,16 +240,16 @@ struct DivingLogView: View {
     
     let book = DivingBook(
         title: "Challenge 2",
-        goal: "디자인 야무지게 하기",
+        goal: "집에 가기",
         startDate: Date(),
         endDate: Calendar.current.date(byAdding: .day, value: 3, to: Date())!
     )
     
     let log = DivingDailyLog(
         date: Date(),
-        todayGoal: "하루 목표",
+        todayGoal: "집에 일찍 가기",
         todayMood: .happy,
-        todayNote: "오늘은 기분이 좋아요!",
+        todayNote: "집에 가고 싶어요",
         book: book
     )
     
